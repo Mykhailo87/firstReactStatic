96924bf9e16c55ccd7ffba87010477a9e47afc20
Mykhailo87 <64310136+Mykhailo87@users.noreply.github.com>
2022-09-06 14:18:24 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react"
-import Info from "./components/Info"
-import About from "./components/About"
-import Interest from "./components/Interest"
-import Footer from "./components/Footer"
+import Info from "./Info"
+import About from "./About"
+import Interest from "./Interest"
+import Footer from "./Footer"
 
 export default function App() {
     return (
