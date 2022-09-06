dcf68d0df86c2c3421465d9ac83c0ec07ef538ae
Mykhailo87 <64310136+Mykhailo87@users.noreply.github.com>
2022-09-06 16:13:17 +0300

    Update Info.js

--- Info.js
+++ Info.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Info() {
     return (
           <div className="info">
-            <img className="info-foto" src="../images/my-foto.jpg" alt="My Foto"></img>
+            <img className="info-foto" src="./my-foto.jpg" alt="My Foto"></img>
             <h3 className="info-name">Mykhailo Ovcharenko</h3>
             <h5 className="info-profesion">Frondend Developer</h5>
             <h6 className="info-web">MyOwn.website</h6>
             <div className="info-button">
-                <img className="but-img"  src="../images/Mail.png"></img>
+                <img className="but-img"  src="./Mail.png"></img>
                 Email
              </div>
           </div>  
