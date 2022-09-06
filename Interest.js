5e3104e21a045daa30b45ae5f8ae88746eea4935
Mykhailo87 <64310136+Mykhailo87@users.noreply.github.com>
2022-09-06 14:04:49 +0300

    Finish Project

---
+++ Interest.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Interest() {
+    return (
+        <div className="interest">
+          <h5>Interests</h5>
+          <p>Food expert. Music scholar.Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek.Pop culture ninja.Coffee fanatic.
+          </p> 
+        </div>
+    )
+} 
